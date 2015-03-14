81c539bcbffe43e9514e75d886ac6f6bb1388aa3
Phr00t <phr00t@gmail.com>
2015-03-14 16:18:01 -0400

    fix pointless WARNING created with spherecollisionshapes

--- jme3-bullet/src/main/java/com/jme3/bullet/collision/shapes/SphereCollisionShape.java
+++ jme3-bullet/src/main/java/com/jme3/bullet/collision/shapes/SphereCollisionShape.java
@@ -91,7 +91,7 @@ public class SphereCollisionShape extends CollisionShape {
 //        new SphereShape(radius);
 //        objectId.setLocalScaling(Converter.convert(getScale()));
 //        objectId.setMargin(margin);
-        setScale(scale);
+        //setScale(scale); //why call setScale if it just causes a warning?
         setMargin(margin);
     }
     
